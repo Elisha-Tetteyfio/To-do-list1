e3f8d1c7dc0ce857e064ddbe20bd10e311912753
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-06-16 13:08:46 +0200

    correct linter errors

--- src/functioning.test.js
+++ src/functioning.test.js
@@ -1,43 +1,42 @@
-import { removve, addTask} from './mock.js';
+import { removve, addTask } from './mock.js';
 
-describe("Test the remove functionality", () => {
+describe('Test the remove functionality', () => {
   test("testing 'remove' function", () => {
-    let allTasks =[1,2,3,4,5];
-    let a = 1;
-    expect(removve(a,allTasks)).toEqual([1,3,4,5])
+    const allTasks = [1, 2, 3, 4, 5];
+    const a = 1;
+    expect(removve(a, allTasks)).toEqual([1, 3, 4, 5]);
   });
 
   test("testing 'remove' function", () => {
-    let allTasks =[
-      {des: "Task 1"},
-      {des: "Task 2"},
-      {des: "Task 3"}
+    const allTasks = [
+      { des: 'Task 1' },
+      { des: 'Task 2' },
+      { des: 'Task 3' },
     ];
-    let a = 1;
-    expect(removve(a,allTasks)).toEqual([{des: "Task 1"}, {des: "Task 3"}])
+    const a = 1;
+    expect(removve(a, allTasks)).toEqual([{ des: 'Task 1' }, { des: 'Task 3' }]);
   });
-})
+});
 
-describe("Test the add functionality", () => {
+describe('Test the add functionality', () => {
   test("testing 'addTask' function", () => {
-    let allTasks =[1,2,3,4,5];
-    let i = {};
-    expect(addTask(i,allTasks)).toEqual([1,2,3,4,5,{index:5}])
+    const allTasks = [1, 2, 3, 4, 5];
+    const i = {};
+    expect(addTask(i, allTasks)).toEqual([1, 2, 3, 4, 5, { index: 5 }]);
   });
-  
+
   test("testing 'addTask' function", () => {
-    let allTasks =[
-      {des: "Task 1"},
-      {des: "Task 2"},
-      {des: "Task 3"}
+    const allTasks = [
+      { des: 'Task 1' },
+      { des: 'Task 2' },
+      { des: 'Task 3' },
     ];
-    let i = {des: "Task 4"};
-    expect(addTask(i,allTasks)).toEqual([
-      {des: "Task 1"},
-      {des: "Task 2"},
-      {des: "Task 3"},
-      {des:"Task 4", index:3}
-    ])
+    const i = { des: 'Task 4' };
+    expect(addTask(i, allTasks)).toEqual([
+      { des: 'Task 1' },
+      { des: 'Task 2' },
+      { des: 'Task 3' },
+      { des: 'Task 4', index: 3 },
+    ]);
   });
-})
-
+});
--- src/mock.js
+++ src/mock.js
@@ -1,10 +1,10 @@
 export const removve = (i, allTasks) => {
   allTasks.splice(i, 1);
-  return(allTasks);
+  return (allTasks);
 };
 
 export const addTask = (a, allTasks) => {
   allTasks.push(a);
   a.index = allTasks.indexOf(a);
-  return(allTasks)
+  return (allTasks);
 };
