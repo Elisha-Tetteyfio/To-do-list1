0e3c2572fe57f30da16d023d979e9b2c55761b91
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-06-16 10:24:30 +0000

    Add mock file for testing adding and removing

---
+++ src/mock.js
@@ -0,0 +1,10 @@
+export const removve = (i, allTasks) => {
+  allTasks.splice(i, 1);
+  return(allTasks);
+};
+
+export const addTask = (a, allTasks) => {
+  allTasks.push(a);
+  a.index = allTasks.indexOf(a);
+  return(allTasks)
+};
